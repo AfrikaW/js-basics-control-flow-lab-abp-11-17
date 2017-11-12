9f0f3be10b184f48abe6170f88390f8e3a215054
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-12 01:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // define paremeters. Then need two different responses.
-const scuberGreetingForFeet
-
+let string
 Function scuberGreetingForFeet(string) {
  if (string <=400) {
  return  "This one is on me!"
